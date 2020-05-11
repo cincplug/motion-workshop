0e6c670647ca377089af88e374beea9a27153dd9
Dave Bitter <dbitter@mirabeau.nl>
2020-05-11 10:43:27 +0200

    feat(carousel): Added basic react spring

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,17 +1,30 @@
 // Libs
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useSpring, animated } from 'react-spring'
 
 // Utils
 
 // Components
 
 // Component
-const CarouselItem = ({ title, copy, image, isActive }) => <li className='carousel__item' data-is-active={isActive}>
-    <h2 className='carousel__title'>{title}</h2>
-    <p className='carousel__copy'>{copy}</p>
-    <img className='carousel__image' src={image} alt={title} />
-</li>
+const CarouselItem = ({ title, copy, image, isActive }) => {
+    const animationProps = useSpring({
+        transform: 'translate3d(0,-40px,0)',
+        opacity: 1,
+        delay: isActive ? 300 : 0,
+        [isActive ? 'from' : 'to']: {
+            transform: 'translate3d(0,0px,0)',
+            opacity: 0
+        }
+    })
+
+    return <animated.li style={animationProps} className='carousel__item' data-is-active={isActive}>
+        <h2 className='carousel__title'>{title}</h2>
+        <p className='carousel__copy'>{copy}</p>
+        <img className='carousel__image' src={image} alt={title} />
+    </animated.li>
+}
 
 const Carousel = ({ items }) => {
     const [activeIndex, setActiveIndex] = useState(0)
